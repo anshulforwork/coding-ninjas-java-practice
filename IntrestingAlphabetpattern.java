78ac493c5a9fdd26c351ff9c4ce6644da4c51d50
Anshul Sahu <anshulforwork@gmail.com>
2023-05-15 05:31:23 +0530

    Pattern1 practice

---
+++ IntrestingAlphabetpattern.java
@@ -0,0 +1,61 @@
+// Code : Interesting Alphabets
+// Send Feedback
+// Print the following pattern for the given number of rows.
+// Pattern for N = 5
+// E
+// DE
+// CDE
+// BCDE
+// ABCDE
+// Input format :
+// N (Total no. of rows)
+// Output format :
+// Pattern in N lines
+// Constraints
+// 0 <= N <= 26
+// Sample Input 1:
+// 8
+// Sample Output 1:
+// H
+// GH
+// FGH
+// EFGH
+// DEFGH
+// CDEFGH
+// BCDEFGH
+// ABCDEFGH
+// Sample Input 2:
+// 7
+// Sample Output 2:
+// G
+// FG
+// EFG
+// DEFG
+// CDEFG
+// BCDEFG
+// ABCDEFG
+
+// ___________________________________________________________________________________________________________________________________________________
+
+import java.util.Scanner;
+
+public class IntrestingAlphabetpattern {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Your class should be named Solution.
+		 * Read input as specified in the question.
+		 * Print output as specified in the question.
+		 */
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print((char)('A'+n-i+j-1));
+			}
+			System.out.println();
+		}
+	}
+
+}
