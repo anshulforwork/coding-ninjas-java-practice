03e48d3ba6139642632983d48be59b7aa765a216
Anshul Sahu <anshulforwork@gmail.com>
2023-05-22 23:18:18 +0530

    22 may practice

--- Trianglenumberpattern.java
+++ Trianglenumberpattern.java
@@ -42,8 +42,8 @@ public class Trianglenumberpattern {
 	 	* Read input as specified in the question.
 	 	* Print output as specified in the question.
 		*/
-    Scanner sc = new Scanner(System.in);
-	int n = sc.nextInt();
+    Scanner sl = new Scanner(System.in);
+	int n = sl.nextInt();
 	
 	for(int i = 1;i<=n;i++){
 		for(int j =1;j<=i;j++)
